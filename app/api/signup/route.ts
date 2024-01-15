9e3b31c3d9facb4c700dd07cbae38d762211b390
shadow16305 <chris16305@gmail.com>
2024-01-15 22:04:11 +0100

    Added Autchentication

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,13 +1,9 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-interface CustomRequest {
-  json: () => Promise<{ email: string; password: string; repeatedPassword: string }>;
-}
-
-export const POST = async (req: CustomRequest) => {
+export const POST = async (req: NextRequest) => {
   try {
     const { email, password, repeatedPassword } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -17,6 +13,6 @@ export const POST = async (req: CustomRequest) => {
 
     return NextResponse.json({ message: "user registered." }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ message: "An error occured while registering the user." }, { status: 500 });
+    return NextResponse.json({ message: "An error occurred while registering the user." }, { status: 500 });
   }
 };
